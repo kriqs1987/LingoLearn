aea497df2a57678ae0959892041d31fccdf240f4
kriqs1987 <krzysiek.kielbon@gmail.com>
2025-11-03 16:13:48 +0100

    feat: Update supported languages

--- constants.ts
+++ constants.ts
@@ -3,15 +3,7 @@ export const QUIZ_SESSION_LENGTH = 5;
 export const LOCAL_STORAGE_KEY = 'lingoLearnData';
 
 export const SUPPORTED_LANGUAGES = [
+    "Norwegian",
     "English",
-    "Polish",
-    "Spanish",
-    "German",
-    "French",
-    "Italian",
-    "Portuguese",
-    "Russian",
-    "Japanese",
-    "Chinese (Simplified)",
-    "Arabic"
+    "Romanian"
 ];
